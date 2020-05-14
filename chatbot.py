7d894d8ea0af7751c827c7f772c1e942a7402e62
SaiVenkat27 <48291098+SaiVenkat27@users.noreply.github.com>
2020-05-14 20:10:13 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -657,11 +657,11 @@ dropout = 0.1
 batch_size = 64
 
 # Set checkpoint to load from; set to None if starting from scratch
-# loadFilename = 4000
+loadFilename = None
 checkpoint_iter = 4000
-loadFilename = os.path.join(save_dir, model_name, corpus_name,
-                           '{}-{}_{}'.format(encoder_n_layers, decoder_n_layers, hidden_size),
-                           '{}_checkpoint.tar'.format(checkpoint_iter))
+# loadFilename = os.path.join(save_dir, model_name, corpus_name,
+#                            '{}-{}_{}'.format(encoder_n_layers, decoder_n_layers, hidden_size),
+#                            '{}_checkpoint.tar'.format(checkpoint_iter))
 
 
 # Load model if a loadFilename is provided
